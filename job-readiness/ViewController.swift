d11722538f27c610f37d4cdf8edf9afb19a03812
Ana Lucia Blanco <analuciablanco@outlook.com>
2022-09-20 20:20:21 -0700

    search bar added

--- job-readiness/ViewController.swift
+++ job-readiness/ViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let searchBarController: UISearchController = {
+        let controller = UISearchController()
+        controller.automaticallyShowsCancelButton = true
+        controller.searchBar.placeholder = "Buscar"
+        return controller
+    }()
+    
+    var button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .customYellow
+        button.setTitle("Test", for: .normal)
+        return button
+    }()
+    
     private let viewModel: UIViewController?
     
     required init?(coder: NSCoder) {
@@ -22,7 +37,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        setupSearchBar()
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        view.addSubview(button)
+        
+        constraints()
+        
+        var isSearchBarEmpty: Bool {
+            return searchBarController.searchBar.text?.isEmpty ?? true
+        }
+    }
+    
+    private func setupSearchBar() {
+        searchBarController.automaticallyShowsScopeBar = true
+        searchBarController.searchBar.delegate = self
+        navigationItem.searchController = searchBarController
+    }
+    
+    private func constraints() {
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    @objc func didTapButton(_ sender: UIButton) {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .systemTeal
+        
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchString = searchBar.text else { return }
+        
+    }
+}
